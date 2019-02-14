0a69f5c9a330d4ebd1b99309e1af3f708933d370
Parthiban Rajendran <parthi2929@gmail.com>
2019-02-14 20:37:58 +0530

    fixing for loop issue in multiple time zones for reference section

--- reference/public/timezone.js
+++ reference/public/timezone.js
@@ -1,76 +1,48 @@
-  // Create a DataSet (allows two way data-binding)
-  var today = vis.moment(vis.moment.utc().format('YYYY-MM-DDT00:00:00.000Z'));
-  var start = today.clone();
-  var end = today.clone().add(2, 'day');
-  var customTime = today.clone().add(28, 'hour');
+// Create a DataSet (allows two way data-binding)
+var today = vis.moment(vis.moment.utc().format('YYYY-MM-DDT00:00:00.000Z'));
+var start = today.clone();
+var end = today.clone().add(2, 'day');
+var customTime = today.clone().add(28, 'hour');
 
-  var items = new vis.DataSet([
-    // {id: 1, content: 'item 1', start: today.clone().add(8, 'hour')},
-    // {id: 2, content: 'item 2', start: today.clone().add(16, 'hour')},
-    // {id: 3, content: 'item 3', start: today.clone().add(32, 'hour')}
-  ]);
+var items = new vis.DataSet([
+  // {id: 1, content: 'item 1', start: today.clone().add(8, 'hour')},
+  // {id: 2, content: 'item 2', start: today.clone().add(16, 'hour')},
+  // {id: 3, content: 'item 3', start: today.clone().add(32, 'hour')}
+]);
 
-
-
-  // Create a timeline displaying in PST
-var timelinePST = new vis.Timeline(document.getElementById('PST'), items, {
-    editable: true,
-    start: start,
-    end: end,
+function getoptions(tzo)
+{
+  var options = {start: start, end:end, 
     moment: function (date) {
-      return moment(date).tz('America/Los_Angeles');  
-    },
-    format: {
-        minorLabels: {
-            hour: 'h:mm'
-        }
+      return moment(date).tz(tzo);
     }
-  });
-  timelinePST.addCustomTime(customTime);
+  }  
+  return options; 
+}
 
-// CST
-var timelineCST = new vis.Timeline(document.getElementById('CST'), items, {
-    editable: true,
-    start: start,
-    end: end,
-    moment: function (date) {
-      return moment(date).tz('America/Regina');  
-    },
-    format: {
-        minorLabels: {
-            hour: 'h:mm'
-        }
-    }
-  });
-  timelineCST.addCustomTime(customTime);
 
-// EST
-var timelineEST = new vis.Timeline(document.getElementById('EST'), items, {
-    editable: true,
-    start: start,
-    end: end,
-    moment: function (date) {
-      return moment(date).tz('America/New_York');  
-    },
-    format: {
-        minorLabels: {
-            hour: 'h:mm'
-        }
-    }
-  });
+// Create a timeline displaying in PST
+tzs = ['America/Los_Angeles', 'America/Regina','America/New_York'];
+IDs = ['PST','CST','EST'];
+tlos = [];
 
-  timelineEST.addCustomTime(customTime);
+for (var i = 0; i<3; i++)
+{
+  tlos[i] = new vis.Timeline(document.getElementById(IDs[i]), items, getoptions(tzs[i]));
+  tlos[i].addCustomTime(customTime);
+}
 
 
 // Create a timeline displaying in local time (default)
-var timelineLocal = new vis.Timeline(document.getElementById('LOCAL'), items, {
-    editable: true,
-    start: start,
-    end: end,
-    format: {
-        minorLabels: {
-            hour: 'h:mm'
-        }
+var options = {
+  editable: true,
+  start: start,
+  end: end,
+  format: {
+    minorLabels: {
+      hour: 'h:mm'
     }
-    });
+  }
+}
+var timelineLocal = new vis.Timeline(document.getElementById('LOCAL'), items, options);
 timelineLocal.addCustomTime(customTime);
